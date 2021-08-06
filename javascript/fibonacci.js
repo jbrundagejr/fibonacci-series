481826a2e8805fd5d9d4487ef7e2451550f59f4c
Jon <jbrundagejr@gmail.com>
2021-08-06 14:08:31 -0400

    done with notes

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,25 +1,20 @@
 function fibonacci(num) {
+  // create an empty array to push our values into
   let fibArr = []
+  // handle edge case of entering 0
   if(num === 0) return 0
+  // iterate through the number given
   for(let i = 0; i < num; i++){
+    // give the array the proper building blocks of [0, 1] to start the Fib sequence
     if(i === 0){
       fibArr.push(0)
       fibArr.push(1)
-    } else 
-    fibArr.push((fibArr[i - 1] + fibArr[i]))
+      // add the previous value to the current iteration value and put it in the Fib Array
+    } else fibArr.push((fibArr[i - 1] + fibArr[i]))
   }
+  // return the value of the given index of our Fibonacci array
   return fibArr[num]
 }
-
-// let nthElement = 0
-// for(let i = 0; i < num; i++){
-//   let firstNumber = i
-//   let secondNumber = i + 1
-//   let nextNumber = firstNumber + secondNumber
-//   firstNumber = secondNumber
-//   nthElement = nextNumber
-// }
-// return nthElement
 
 if (require.main === module) {
   // add your own tests in here
