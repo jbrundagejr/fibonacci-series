7eca6335c02367ef2e40ebdbd604032dfdd6c4c0
Jon <jbrundagejr@gmail.com>
2021-08-06 13:59:56 -0400

    done

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,6 +1,25 @@
 function fibonacci(num) {
-  // type your code here
+  let fibArr = []
+  if(num === 0) return 0
+  for(let i = 0; i < num; i++){
+    if(i === 0){
+      fibArr.push(0)
+      fibArr.push(1)
+    } else 
+    fibArr.push((fibArr[i - 1] + fibArr[i]))
+  }
+  return fibArr[num]
 }
+
+// let nthElement = 0
+// for(let i = 0; i < num; i++){
+//   let firstNumber = i
+//   let secondNumber = i + 1
+//   let nextNumber = firstNumber + secondNumber
+//   firstNumber = secondNumber
+//   nthElement = nextNumber
+// }
+// return nthElement
 
 if (require.main === module) {
   // add your own tests in here
